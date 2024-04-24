d5375de13b202f296ee2153a09ec079e9207c78f
HiYUNo <jayhyun1008@gmail.com>
2024-04-24 11:41:52 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const LANGUAGE = 'ko-KR'
 
 // 테마 색상
-const THEMECOLOR = "#448EE4"
-const BACKIMGURL = "./assets/bground.jpeg"
+const THEMECOLOR = "#353d87"
+const BACKIMGURL = "https://s3.lapy.link/klapy/75a1ed8c-d0b1-45ae-b07e-54dc73e85941.webp"
 
 // 페이지 json을 이용하는 특성상 마스토돈은 지원하지 않습니다. 양해부탁드려요!
-const MISSKEYUSER = "CabinetKey" // 미스키 유저네임 (대소문자 구분)
-const MISSKEYHOST = "i.peacht.art" // 미스키 호스트
+const MISSKEYUSER = "hyun1008" // 미스키 유저네임 (대소문자 구분)
+const MISSKEYHOST = "k.lapy.link" // 미스키 호스트
