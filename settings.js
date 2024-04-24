42cdd20f12134bce99bea8d1a169820b3a879306
HiYUNo <jayhyun1008@gmail.com>
2024-04-24 14:58:12 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,4 +6,4 @@ const BACKIMGURL = "https://s3.lapy.link/klapy/75a1ed8c-d0b1-45ae-b07e-54dc73e85
 
 // 페이지 json을 이용하는 특성상 마스토돈은 지원하지 않습니다. 양해부탁드려요!
 const MISSKEYUSER = "hyun1008" // 미스키 유저네임 (대소문자 구분)
-const MISSKEYHOST = "k.lapy.link" // 미스키 호스트
+const MISSKEYHOST = "gwansangg.am" // 미스키 호스트
