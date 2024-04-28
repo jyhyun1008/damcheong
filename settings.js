1c184c9bd70d30fbcd591aaeba781ec1b28f2af3
HiYUNo <jayhyun1008@gmail.com>
2024-04-28 20:18:54 +0900

    bg color chang e

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const LANGUAGE = 'ko-KR'
 
 // 테마 색상
-const THEMECOLOR = "#353d87"
+const THEMECOLOR = "#283c5f"
 const BACKIMGURL = "https://s3.lapy.link/klapy/75a1ed8c-d0b1-45ae-b07e-54dc73e85941.webp"
 
 // 페이지 json을 이용하는 특성상 마스토돈은 지원하지 않습니다. 양해부탁드려요!
